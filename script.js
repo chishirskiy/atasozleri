6515d18b1523518642723bca3cfe8a609b4cda05
chishirskiy <mr.lime1000@gmail.com>
2026-07-31 02:26:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ fetch("quotes.json")
         const search = document.getElementById("search");
 
         function preview(text) {
-            return text.substring(0, 220);
+            return "";
         }
 
         function render(list) {
